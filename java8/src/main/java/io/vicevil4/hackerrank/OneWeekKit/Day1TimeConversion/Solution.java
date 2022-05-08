cbe3e5e9802b31b63b744f24d2afa04a3eef46f3
Simon Jung <vicevil4@gmail.com>
2022-05-09 00:08:16 +0900

    challenges/one-week-preparation-kit-time-conversion

---
+++ java8/src/main/java/io/vicevil4/hackerrank/OneWeekKit/Day1TimeConversion/Solution.java
@@ -0,0 +1,59 @@
+package io.vicevil4.hackerrank.OneWeekKit.Day1TimeConversion;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * https://www.hackerrank.com/challenges/one-week-preparation-kit-time-conversion/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one&h_r=next-challenge&h_v=zen
+ */
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+        // Write your code here
+        int hour = Integer.parseInt(s.substring(0,2));
+        String part = s.substring(2,8);
+        String meridiem = s.substring(8,10);
+        if ("PM".equals(meridiem)) {
+            if (hour == 24) hour = 0;
+            else if (hour != 12) hour = hour + 12;
+        } else { // AM
+            if (hour == 12) hour = 0;
+        }
+        return String.format("%02d%s", hour, part);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
