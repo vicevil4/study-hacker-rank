3311f0957ca23fa5ba54eb80fb274655693a9a8b
Simon Jung <vicevil4@gmail.com>
2022-05-08 23:14:49 +0900

    challenges/one-week-preparation-kit-plus-minus

--- java8/src/main/java/io/vicevil4/hackerrank/OneWeekKit/Day1PlusMinus/Solution.java
+++ java8/src/main/java/io/vicevil4/hackerrank/OneWeekKit/Day1PlusMinus/Solution.java
@@ -1,4 +1,55 @@
 package io.vicevil4.hackerrank.OneWeekKit.Day1PlusMinus;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * https://www.hackerrank.com/challenges/one-week-preparation-kit-plus-minus/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+ */
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+        final float minimumValue = 1.0f / arr.size();
+        float positiveProportion = 0.0f;
+        float negativeProportion = 0.0f;
+        float zeroProportion = 0.0f;
+        for (Integer value : arr) {
+            if (value > 0) positiveProportion += minimumValue;
+            else if (value < 0) negativeProportion += minimumValue;
+            else zeroProportion += minimumValue;
+        }
+        System.out.printf("%.6f%n", positiveProportion);
+        System.out.printf("%.6f%n", negativeProportion);
+        System.out.printf("%.6f%n", zeroProportion);
+    }
+
+}
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
 }
