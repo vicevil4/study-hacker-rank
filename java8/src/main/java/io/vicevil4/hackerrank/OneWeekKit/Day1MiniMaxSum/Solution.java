97ea7639dcdd230db6f4a85075cbf7826708ef2b
Simon Jung <vicevil4@gmail.com>
2022-05-08 23:43:09 +0900

    challenges/one-week-preparation-kit-mini-max-sum

---
+++ java8/src/main/java/io/vicevil4/hackerrank/OneWeekKit/Day1MiniMaxSum/Solution.java
@@ -0,0 +1,51 @@
+package io.vicevil4.hackerrank.OneWeekKit.Day1MiniMaxSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+ */
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        List<Integer> sortedArr = arr.stream().sorted().collect(toList());
+        int count = sortedArr.size();
+        Long partialSum = 0L;
+        for (int ii = 1; ii < count - 1; ii++) {
+            partialSum += sortedArr.get(ii);
+        }
+
+        System.out.printf("%d %d%n"
+                , partialSum + sortedArr.get(0)
+                , partialSum + sortedArr.get(count - 1));
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
